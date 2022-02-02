167eb458896707f0a6fe7e457feaf6a9878d8d39
Felipe Castro <luisfelipecs97@gmail.com>
2022-02-02 11:19:21 -0600

    Modified a new Algorithm in Java

---
+++ JavaScript/Algorithms/Hacker Rank/ZigZagSequence.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.math.*;
+
+public class ZigZagSequence {
+
+	public static void main(String[] args) throws java.lang.Exception {
+		int a[] = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+
+		findZigZagSequence(a, 7);
+
+	}
+
+    public static void findZigZagSequence(int[] a, int n) {
+		Arrays.sort(a);
+		int mid = ((n + 1) / 2) - 1; // 1
+		int temp = a[mid];
+		a[mid] = a[n - 1];
+		a[n - 1] = temp;
+
+		int st = mid + 1;
+		int ed = n - 2; // 2
+		while (st < ed) {
+			temp = a[st];
+			a[st] = a[ed];
+			a[ed] = temp;
+			st = st + 1;
+			ed = ed - 1; // 3
+		}
+		for (int i = 0; i < n; i++) {
+			if (i > 0)
+				System.out.print(" ");
+			System.out.print(a[i]);
+		}
+	}
+
+
+
+
+}
